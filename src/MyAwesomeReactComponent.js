78cdeb8f1e1ec440611dfe0da06bcc9828ab8efb
MilanBrkicFON <milan.brkic1@yahoo.com>
2018-02-03 11:30:34 +0100

    init

---
+++ src/MyAwesomeReactComponent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const MyAwesomeReactComponent = () => (
+    <RaisedButton label="Default" onClick={() => alert('Hello World! ')}/>
+);
+
+export default MyAwesomeReactComponent;
