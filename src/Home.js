eb49d5598951cbb5450c51c157c9bf27390dbbf8
MilanBrkicFON <milan.brkic1@yahoo.com>
2018-03-19 00:10:17 +0100

    get Places from back end

---
+++ src/Home.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './index.css';
+import App from './App';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
+import Profile from './profile';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+export default class Home extends React.Component {
+	render() {
+		return (
+			<MuiThemeProvider>
+				<Router>
+					<div>
+						<div className="navigation">
+							<AppBar
+								iconElementLeft={
+									<div>
+										<Link to="/"><FlatButton label="Home" /></Link>
+										<Link to="/profile"><FlatButton label="Profile" /></Link>
+									</div>
+								}
+								iconElementRight={<FlatButton label="Login"/>}
+							/>
+						</div>
+
+						<Switch>
+							<Route exact path="/" component={App}/>
+							<Route exact path="/profile" component={Profile}/>
+						</Switch>
+					</div>
+				</Router>
+			</MuiThemeProvider>
+		);
+	}
+}
---
+++ src/profile/Profile.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {List, ListItem} from 'material-ui/List';
+import Subheader from 'material-ui/Subheader';
+import Avatar from 'material-ui/Avatar';
+import fetch from 'node-fetch';
+import * as config from '.././config';
+import * as endpoint from '../datasource/endpoint';
+
+export default class Profile extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state ={
+			cities: []
+		}
+	}
+
+	componentDidMount() {
+		fetch(config.admin + endpoint.city.base + endpoint.city.getAll)
+			.then(response => response.json())
+			.then(data => this.setState({cities: data}))
+	}
+	render() {
+		return (
+			this.state.cities &&
+			<div>
+				<List className="cities-list">
+					<Subheader> Cities </Subheader>
+					{this.state.cities.map(city =>
+						<ListItem primaryText={city.name}
+											leftAvatar={(<Avatar> {city.name[0]} </Avatar>)}
+						/>
+					)}
+				</List>
+			</div>
+		);
+	}
+}
